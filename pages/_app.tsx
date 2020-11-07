9515e48d3c8fa275f564c8a63c1ec222faec5035
Art Rosnovsky <art.rosnovsky@auth0.com>
2020-11-06 19:26:52 -0800

    added Fathom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
 import { AppProps } from 'next/app'
 import '../styles/index.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as Fathom from 'fathom-client'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    Fathom.load('UHVHKTPD', {
+      includedDomains: ['https://rosnovskyus.vercel.app']
+    })
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview()
+    }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  }, [])
   return <Component {...pageProps} />
 }
